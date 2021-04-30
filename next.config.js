befdaa85d8ce723dd8fbb913e8c948b914c449e0
Romulo Messias <romulomessias@me.com>
2021-04-29 21:14:08 -0300

    chore: add support to webpack 5 and a .env file exemple

--- next.config.js
+++ next.config.js
@@ -6,9 +6,7 @@ console.log("hey =>", process.env.SPOTIFY_CLIENT_ID);
 
 module.exports = {
     target: "serverless",
-
-    // webpack: (config, { webpack }) => {
-    //     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-    //     return config;
-    // },
+    future: {
+        webpack5: true,
+    },
 };
