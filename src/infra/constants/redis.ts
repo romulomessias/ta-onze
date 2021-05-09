0dd090640084fe1d25e33609739053655636309e
Romulo Messias <romulomessias@me.com>
2021-05-09 16:55:34 -0300

    fix: rename refresh token key

--- src/infra/constants/redis.ts
+++ src/infra/constants/redis.ts
@@ -2,5 +2,5 @@ const suffix =
     process.env.NODE_ENV !== "production" ? `-${process.env.NODE_ENV}` : "";
 
 export const tokenKey = "playOnze" + suffix;
-export const refreshTokenKey = "replayPlayOnze" + suffix;
+export const refreshTokenKey = "replayOnze" + suffix;
 export const currentSprintKey = "currentSprint" + suffix;
