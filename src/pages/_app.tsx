1c57f32dd295e717889e0a8df75b1eff0e856b8e
Romulo <romulomessias@me.com>
2021-06-10 09:46:30 -0300

    faet: add script to install service worker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,11 @@ const App: FC<IAppProps> = ({ Component, pageProps, renderer }) => {
                     href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;500;700&display=swap"
                     rel="stylesheet"
                 />
+                <script type="module">
+                    import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+                    const el = document.createElement('pwa-update');
+                    document.body.appendChild(el);
+                </script>
             </Head>
             <FelaProvider renderer={renderer}>
                 <Component {...pageProps} />
