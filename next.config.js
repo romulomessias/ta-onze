f13216a9a8f16fa37335a63c51acda0d7de41b65
Romulo <hello@romulomessias.dev>
2022-02-18 17:36:55 -0300

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ dotenv.config();
 const { SLACK_CLIENT_ID } = process.env;
 
 module.exports = {
-    target: "serverless",
     webpack5: true,
     images: {
         domains: ['mosaic.scdn.co', 'i.scdn.co'],
