f5bdd066f59b498a2ef8d35e0bf1d56ae3c7121f
Romulo <hello@romulomessias.dev>
2022-04-02 14:19:45 -0300

    chore: update dependencies

---
+++ global.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="node" />
+
+declare namespace NodeJS {
+    interface Process {
+        browser: boolean;
+    }
+
+    interface ProcessEnv {
+        NODE_ENV: "development" | "production";
+        PUBLIC_URL: string;
+
+        SPOTIFY_CLIENT_ID: string;
+        SPOTIFY_SECRET_CLIENT_ID: string;
+
+        TAONZE_AWS_ACCESS_KEY_ID: string;
+        TAONZE_AWS_SECRET_ACCESS_KEY: string;
+    }
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,26 +1,5 @@
-/// <reference types="node" />
 /// <reference types="next" />
-/// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-declare namespace NodeJS {
-    interface Process {
-        browser: boolean;
-    }
-
-    interface ProcessEnv {
-        NODE_ENV: "development" | "production";
-        PUBLIC_URL: string;
-
-        SPOTIFY_CLIENT_ID: string;
-        SPOTIFY_SECRET_CLIENT_ID: string;
-
-        REDIS_ACCESS: string;
-
-        SLACK_CLIENT_ID: string;
-
-        TAONZE_AWS_ACCESS_KEY_ID: string;
-        TAONZE_AWS_SECRET_ACCESS_KEY: string;
-        AWS_DYNAMO_TABLE_NAME: string;
-    }
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
