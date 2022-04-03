a0fb3105609c933984ba6604e95949cac48c26ce
Romulo <hello@romulomessias.dev>
2022-04-03 12:26:06 -0300

    chore: update lambda function

--- src/lambdas/processContributorProfile.js
+++ src/lambdas/processContributorProfile.js
@@ -1,16 +1,48 @@
 const https = require("https");
 
+const postProfile = (contributorId) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            hostname: "www.taonze.com.br",
+            path: "/api/contributors/" + contributorId,
+            method: "POST",
+            port: 443,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        const req = https.request(options, (res) => {
+            let rawData = "";
+
+            res.on("data", (chunk) => {
+                rawData += chunk;
+            });
+
+            res.on("end", () => {
+                ``;
+                resolve(JSON.parse(rawData));
+            });
+        });
+
+        req.on("error", (err) => {
+            reject(new Error(err));
+        });
+        req.write(JSON.stringify({ contributorId }));
+        req.end();
+    });
+};
 exports.handler = async (event, context) => {
     const [record] = event.Records;
     const contributorId = record.messageAttributes.Id.stringValue;
-    console.log({ contributorId });
-    console.log("processContributorProfile :D");
 
-    https.post("https://www.taonze.com.br/api/contributors/" + contributorId);
-    // .then((response) => {
-    //     console.log("deu bom", response);
-    // })
-    // .catch((e) => {
-    //     console.error("deu ruim", e);
-    // });
+    console.log("processContributorProfile", contributorId);
+    try {
+        const result = await postProfile(contributorId);
+        console.log("got profile for", contributorId, { result });
+        return { contributorId };
+    } catch (err) {
+        console.error("something got wrong", contributorId, err);
+        return { err };
+    }
 };
